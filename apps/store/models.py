1d06bf17e2ea57c48c490a0a43f4f9ab6685db7e
andersonsimplicio <anderson25eu@gmail.com>
2026-09-22 02:16:39 -0300

    Django Ninja Banco de Dados Criado

---
+++ apps/store/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Produto(models.Model):
+    nome =  models.CharField(max_length=100)
+    descicao = models.TextField()
+    preco =  models.DecimalField(max_digits=10, decimal_places=2)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Nome: {self.nome} "
+    
+class Ordem(models.Model):
+    usuario= models.ForeignKey(User, on_delete=models.CASCADE)
+    produto = models.ManyToManyField(Produto, through="OrdemItem")
+    total = models.DecimalField(max_digits=10,decimal_places=2)
+    status = models.CharField(max_length=20, default='pendencia')
+    criado_em = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Ordem #{self.id} - {self.usuario.username}"
+    
+
+class OrdemItem(models.Model):
+    ordem = models.ForeignKey(Ordem, on_delete=models.CASCADE)
+    produto =  models.ForeignKey(Produto,on_delete=models.CASCADE)
+    quantidade = models.IntegerField(default=1)
+    preco = models.DecimalField(max_digits=10,decimal_places=2)
+    
+    def __str__(self):
+        return f"{self.quantidade}x {self.produto.nome} (Ordem #{self.ordem.id})"
